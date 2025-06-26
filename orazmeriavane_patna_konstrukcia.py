3d1ce199fcb84f98bd4f42c27462915417061ab4
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-06-26 15:06:41 +0300

    Update orazmeriavane_patna_konstrukcia.py

--- orazmeriavane_patna_konstrukcia.py
+++ orazmeriavane_patna_konstrukcia.py
@@ -292,7 +292,7 @@ else:
                     left: 50%;
                     transform: translate(-50%, -50%);
                     font-size: 20px;
-                    font-weight: bold;
+                    font-weight: normal;
                 ">
                     Ei = {Ei:.2f} MPa
                 </div>
