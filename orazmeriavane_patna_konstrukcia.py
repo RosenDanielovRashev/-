649c9e570709a0984122689102a8ee990b0e227c
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-06-26 15:47:19 +0300

    Update orazmeriavane_patna_konstrukcia.py

--- orazmeriavane_patna_konstrukcia.py
+++ orazmeriavane_patna_konstrukcia.py
@@ -225,7 +225,7 @@ if mode == "Ed / Ei":
                     <div style="
                         position: absolute;
                         top: 50%;
-                        right: 60px;
+                        right: 0px;
                         transform: translateY(-50%);
                         font-size: 16px;
                         color: black;
