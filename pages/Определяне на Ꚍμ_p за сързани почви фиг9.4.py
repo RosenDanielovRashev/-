4f7e45f28605c406bd75de115bf9fa8d173dedc4
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-10 16:25:09 +0300

    Update Определяне на Ꚍμ_p за сързани почви фиг9.4.py

--- pages/Определяне на Ꚍμ_p за сързани почви фиг9.4.py
+++ pages/Определяне на Ꚍμ_p за сързани почви фиг9.4.py
@@ -174,6 +174,7 @@ def plot_tau_b(fi_value, h_value):
         
         ax.plot([x_low, x_high], [y_tau, y_tau], 'g--', alpha=0.6)
         ax.plot(x_value, y_tau, 'ko', markersize=8, 
+                label=f'τb = {y_tau:.6f}\nH: {h_low}→{h_value}→{h_high}\nφ: {fi_low}→{fi_value}→{fi_high}')
 
         # Добавяне на текста в долния ляв ъгъл (координати [0, 0] = долен ляв ъгъл)
         ax.text(0.02, 0.02,  # x, y (в относителни координати, 0-1)
