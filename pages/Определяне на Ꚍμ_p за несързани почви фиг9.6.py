e42331cd2759a76cd5cc64a64db5c26a5b8350fe
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-11 11:35:12 +0300

    Update Определяне на Ꚍμ_p за несързани почви фиг9.6.py

--- pages/Определяне на Ꚍμ_p за несързани почви фиг9.6.py
+++ pages/Определяне на Ꚍμ_p за несързани почви фиг9.6.py
@@ -518,10 +518,10 @@ if point_on_esr_eo is not None:
         ))
         fig.add_trace(go.Scatter(
             x=[x_orange, x_orange],
-            y=[y_red, 1.00],
+            y=[y_red, 1.05],
             mode='lines',
             line=dict(color='orange', dash='dash'),
-            name='Вертикална линия до y=1.00'
+            name='Вертикална линия до y=1.05'
         ))
 
 # Настройка на графиката
