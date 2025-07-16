e82929260af8500bcb2850e043a00c75917cf6f7
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-16 11:23:37 +0300

    Update orazmeriavane_patna_konstrukcia.py

--- orazmeriavane_patna_konstrukcia.py
+++ orazmeriavane_patna_konstrukcia.py
@@ -977,7 +977,7 @@ def generate_pdf_report(include_main, include_fig94, include_fig96, include_fig9
         for img_file in local_images:
             if os.path.exists(img_file):
                 try:
-                    pdf.image(img_file, x=10, w=160)
+                    pdf.image(img_file, x=10, w=150)
                     pdf.ln(2)
                 except:
                     pdf.cell(0, 8, f'Грешка при вмъкване на {img_file}', 0, 1)
