194cd34239c42a2173bea5f76e7e039471a83e72
RosenDanielovRashev <rosenrashev98@gmail.com>
2026-01-08 13:56:54 +0200

    Update опън за междиннен плст.py

--- pages/опън за междиннен плст.py
+++ pages/опън за междиннен плст.py
@@ -359,7 +359,7 @@ if layer_idx in st.session_state.layer_results:
                             mode='text',
                             text=[f'{round(sr_Ei,2)}'],
                             textposition='middle left',
-                            textfont=dict(size=14, color=color),
+                            textfont=dict(size=14, color='black'),
                             showlegend=False,
                             hoverinfo='skip'
                         ))
